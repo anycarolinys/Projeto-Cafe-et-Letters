76f24216583d9e0a66e2b9e519440b7150e7bb7c
leticiacena <leticia.cenaps@hotmail.com>
2021-07-18 14:30:58 -0300

    Add files via upload

---
+++ test/java/control/ControleClienteTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author leticiacena
+ */
+public class ControleClienteTest {
+    
+    public ControleClienteTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of adicionarEventos method, of class ControleCliente.
+     */
+    @Test
+    public void testAdicionarEventos() {
+        System.out.println("adicionarEventos");
+        ControleCliente instance = null;
+//        instance.adicionaEventosEstoque();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
